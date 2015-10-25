ea4ad424ab396fbb9d7c3f69138bf00db168c2c8
Paul Kirth <pk1574@gmail.com>
2015-10-25 02:05:12 -0700

    Improvements to client and sender programs
    now they:
    1. use commandline args
    2. send and receive based on number of args
    
    ToDo:
    improve interface and arg processing

--- client_server/client.c
+++ client_server/client.c
@@ -5,59 +5,97 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-//number of bytes in the TCP payload
+// number of bytes in the TCP payload
 #define PAYLOAD 1024
 
-//number of payloads to send
+// number of payloads to send
 #define TOTAL 75000
 
 int send_data(const char* ipaddr, int port, int size)
 {
-	int fd;
-	int n;
-	char data[PAYLOAD] = {0};
-	struct sockaddr_in server;
-	//struct sockaddr_in client;
+    printf("send_data\n");
+    int fd;
+    int n;
+    char data[PAYLOAD] = {0};
+    struct sockaddr_in server;
+    // struct sockaddr_in client;
 
-	printf("Start %s\n", ipaddr);
+    printf("Start %s\n", ipaddr);
 
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr(ipaddr);
-	server.sin_port = htons(port);
+    fd                     = socket(AF_INET, SOCK_STREAM, 0);
+    server.sin_family      = AF_INET;
+    server.sin_addr.s_addr = inet_addr(ipaddr);
+    server.sin_port        = htons(port);
 
-	connect(fd, (struct sockaddr*) &server, sizeof(server));
-	//15625 packets to send
+    connect(fd, (struct sockaddr*)&server, sizeof(server));
+    // 15625 packets to send
 
-	int i = 0;
-	for(;i < size; ++i)
-	{
-		n = sendto(fd, data, PAYLOAD, 0, (struct sockaddr*)&server, sizeof(server));
-		if(n < 0)
-		{
-			perror("Error sending data");
-		}
-	}
-	printf("Done %s\n", ipaddr);
-	close(fd);
-	return 0;
+    int i = 0;
+    for(; i < size; ++i)
+    {
+        n = sendto(fd, data, PAYLOAD, 0, (struct sockaddr*)&server, sizeof(server));
+        if(n < 0)
+        {
+            perror("Error sending data");
+        }
+    }
+    printf("Done %s\n", ipaddr);
+    close(fd);
+    return 0;
 }
 
+int recv_data(const char* ipaddr, int port, int size)
+{
+    printf("recv_data\n");
+    int fd;
+    int n;
+    char data[PAYLOAD] = {0};
+    struct sockaddr_in server;
+    socklen_t server_len;
+    // struct sockaddr_in client;
+
+    printf("Start %s\n", ipaddr);
+
+    fd                     = socket(AF_INET, SOCK_STREAM, 0);
+    server.sin_family      = AF_INET;
+    server.sin_addr.s_addr = inet_addr(ipaddr);
+    server.sin_port        = htons(port);
+    server_len             = sizeof(server);
+
+    connect(fd, (struct sockaddr*)&server, server_len);
+    // 15625 packets to send
+
+    int i = 0;
+    for(; i < size; ++i)
+    {
+        // n = recvfrom(fd, data, sizeof(data), 0, struct sockaddr *restrict __addr, socklen_t *restrict __addr_len)
+        n = recvfrom(fd, data, PAYLOAD, 0, (struct sockaddr*)&server, &server_len);
+        if(n < 0)
+        {
+            perror("Error receiving data");
+            exit(-1);
+        }
+    }
+    printf("Done %s\n", ipaddr);
+    close(fd);
+    return 0;
+}
 
 
 int main(int argc, char* argv[])
 {
-	//char* str = argv[1];
+    int (*func)(const char*, int, int);
+    func = argc > 2 ? recv_data : send_data;
 
-	if(fork() == 0)
-	{
-		sleep(3);
-		send_data(argv[1], 9876, TOTAL);
-	}
-	else
-	{
-		send_data(argv[1], 48698, TOTAL);
-	}
+    if(fork() != 0)
+    {
+        sleep(3);
+        func(argv[1], 9876, TOTAL);
+    }
+    else
+    {
+        func(argv[1], 48698, TOTAL);
+    }
 
-	return 0;
+    return 0;
 }
--- client_server/server.c
+++ client_server/server.c
@@ -4,9 +4,12 @@
 #include <sys/prctl.h>
 #include <signal.h>
 #include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
 
 int receiver(int port)
 {
+    printf("Receiver\n");
     int connfd, listenfd, n;
     struct sockaddr_in server = {0};
     struct sockaddr_in client = {0};
@@ -14,31 +17,37 @@ int receiver(int port)
     pid_t childpid;
     char msg[1500];
 
-    listenfd=socket(AF_INET, SOCK_STREAM,0);
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr= htonl(INADDR_ANY);
-    server.sin_port= htons(port);
+    listenfd               = socket(AF_INET, SOCK_STREAM, 0);
+    server.sin_family      = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(port);
     bind(listenfd, (struct sockaddr*)&server, sizeof(server));
 
     listen(listenfd, 1024);
 
     for(;;)
     {
-         client_len = sizeof(client);
-         connfd = accept(listenfd, (struct sockaddr*)&client, &client_len);
+        client_len = sizeof(client);
+        connfd     = accept(listenfd, (struct sockaddr*)&client, &client_len);
 
-         if((childpid = fork())==0)
-         {
-              for(;;)
-              {
-                   n = recvfrom(connfd, msg, 1500, 0, (struct sockaddr*)&client, &client_len);
-                   if(n < 0){
-                       perror("Problem receiving data");
-                       break;
-                   }
-              }
-         }
-         close(connfd);
+        if((childpid = fork()) == 0)
+        {
+            for(;;)
+            {
+                n = recvfrom(connfd, msg, 1500, 0, (struct sockaddr*)&client, &client_len);
+                if(n < 0)
+                {
+                    if(errno == ECONNRESET)
+                    {
+                        close(connfd);
+                        return 0;
+                    }
+                    perror("Problem receiving data");
+                    exit(-1);
+                }
+            }
+        }
+        close(connfd);
     }
     close(listenfd);
 
@@ -46,17 +55,68 @@ int receiver(int port)
 }
 
 
-int main()
+int sender(int port)
 {
-    pid_t child = fork();
-    if(child ==0)
+    printf("Sender\n");
+    int connfd, listenfd, n;
+    struct sockaddr_in server = {0};
+    struct sockaddr_in client = {0};
+    socklen_t client_len;
+    pid_t childpid;
+    char msg[1500];
+
+    listenfd               = socket(AF_INET, SOCK_STREAM, 0);
+    server.sin_family      = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(port);
+    bind(listenfd, (struct sockaddr*)&server, sizeof(server));
+
+    listen(listenfd, 1024);
+
+    for(;;)
     {
-        receiver(9876);
+        client_len = sizeof(client);
+        connfd     = accept(listenfd, (struct sockaddr*)&client, &client_len);
+
+        if((childpid = fork()) == 0)
+        {
+            for(;;)
+            {
+                n = sendto(connfd, msg, 1500, 0, (struct sockaddr*)&client, client_len);
+                if(n < 0)
+                {
+                    if(errno == ECONNRESET)
+                    {
+                        perror("connection closed normally... probably");
+                        close(connfd);
+                        return 0;
+                    }
+                    perror("Problem receiving data");
+                    exit(-1);
+                }
+            }
+        }
+        close(connfd);
+    }
+    close(listenfd);
+
+    return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+
+    int (*func)(int);
+    func        = argc > 1 ? sender : receiver;
+    pid_t child = fork();
+    if(child == 0)
+    {
+        func(9876);
         prctl(PR_SET_PDEATHSIG, SIGHUP);
     }
     else
     {
-         receiver(48698);
+        func(48698);
     }
-
 }
